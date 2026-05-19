bcd1343d969a5892b103e72b78f54391b2b1e518
SenseiTarzan <gabrielcaptari0@gmail.com>
2026-05-19 12:35:17 +0200

    P1 work but go to test

---
+++ script/server.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+apt update 
+apt install curl -y
+curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110 --bind-address=192.168.56.110 --advertise-address=192.168.56.110" sh -
+
+# Get the token for the worker nodes
+TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
+mkdir /vagrant/.shut
+# Store the token for the workers to use
+echo $TOKEN > /vagrant/.secret/token
