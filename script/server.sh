5cda2c701f0a4cc88827e2888a1ab40c86701d25
SenseiTarzan <gabrielcaptari0@gmail.com>
2026-05-19 12:45:18 +0200

    add -p

--- script/server.sh
+++ script/server.sh
@@ -5,6 +5,6 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server --node-ip=192.168.56.110
 
 # Get the token for the worker nodes
 TOKEN=$(sudo cat /var/lib/rancher/k3s/server/node-token)
-mkdir /vagrant/.secret
+mkdir -p /vagrant/.secret
 # Store the token for the workers to use
 echo $TOKEN > /vagrant/.secret/token
