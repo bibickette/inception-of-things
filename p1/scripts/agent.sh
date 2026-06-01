a1ce60bdda3fe1455161e283e75eac637486f8c8
bibickette <imm.taly@gmail.com>
2026-06-01 12:14:19 +0200

    fix(p1) : correcting create secret token folder

--- p1/scripts/agent.sh
+++ p1/scripts/agent.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-
-if [ -f /vagrant/.secret/token ]; do
-  rm -rf /vagrant/.secret/token
-done
 apt update 
 apt install curl -y
 
@@ -11,5 +7,8 @@ until [ -f /vagrant/.secret/token ]; do
   echo "not found /vagrant/.secret/token"
   sleep 3
 done
+
+echo "======== SECRET TOKEN FOLDER FOUNDED ========"
 TOKEN=$(cat /vagrant/.secret/token)
+
 curl -sfL https://get.k3s.io | K3S_URL="https://192.168.56.110:6443" K3S_TOKEN="$TOKEN" INSTALL_K3S_EXEC="agent --node-ip=192.168.56.111" sh -
