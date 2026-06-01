8631c702acf7849e7af40c80a4e03411ff959b68
bibickette <imm.taly@gmail.com>
2026-06-01 12:23:51 +0200

    refacto(p2): add echo in setup.sh

--- p2/scripts/setup.sh
+++ p2/scripts/setup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 
+echo "======== DELETING NAMESPACE IF EXISTS ========"
 kubectl delete namespace app-iot
+
+
 kubectl create namespace app-iot
 
 kubectl apply -f app1.yml
@@ -8,4 +11,5 @@ kubectl apply -f app2.yml
 kubectl apply -f app3.yml
 
 kubectl apply -f ingress.yml
+
 kubectl describe ingress app-iot-ingress -n app-iot
