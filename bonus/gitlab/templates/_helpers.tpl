df8ea67a734486097a2c7ee0aec16afe030a5e5a
Luma-3 <jb.brousse@luma3.dev>
2026-06-03 18:23:05 +0200

    refacto(gitlab): refacto helm chart

--- bonus/gitlab/templates/_helpers.tpl
+++ bonus/gitlab/templates/_helpers.tpl
@@ -1,11 +1,11 @@
-{{- define "test.labels" -}}
+{{- define "gitlab.labels" -}}
 app.kubernetes.io/name: {{ .Chart.Name }}
 app.kubernetes.io/instance: {{ .Release.Name }}
 app.kubernetes.io/version: {{ .Chart.AppVersion }}
 app.kubernetes.io/managed-by: {{ .Release.Service }}
 {{- end }}
 
-{{- define "test.selectorLabels" -}}
+{{- define "gitlab.selectorLabels" -}}
 app.kubernetes.io/name: {{ .Chart.Name }}
 app.kubernetes.io/instance: {{ .Release.Name }}
 {{- end }}
--- bonus/setup.sh
+++ bonus/setup.sh
@@ -7,7 +7,13 @@ kubectl delete namespace gitlab
 echo "======== CREATING NAMESPACE IF NOT EXISTS ========"
 kubectl create namespace gitlab
 
-kubectl -n gitlab create secret generic psql-password \
-  --from-literal=password='TonMotDePassePostgres'
+helm upgrade --install gitlab ./gitlab --namespace gitlab --create-namespace --wait
+kubectl wait --for=condition=available deployment/gitlab \
+  -l app.kubernetes.io/name=gitlab  \
+  -n gitlab \
+  --timeout=600s
 
-helm install gitlab gitlab/gitlab -n gitlab -f custom-values.yml
+echo "Password is : "
+GITLAB_POD_NAME=$$(kubectl get pods -n gitlab -l app.kubernetes.io/name=gitlab -o jsonpath={.items[0].metadata.name})
+kubectl exec -n gitlab $GITLAB_POD_NAME -- cat /etc/gitlab/initial_root_password
+echo ""
