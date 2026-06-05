e822d8b21f9a0c55b2df86b90162f2493339471a
Luma-3 <jb.brousse@luma3.dev>
2026-06-05 12:26:44 +0200

    feat(gitlab): gitlab deploy when it's running on localhost

--- bonus/setup.sh
+++ bonus/setup.sh
@@ -1,19 +1,12 @@
 #!/bin/sh
 
 echo "======== DELETING NAMESPACE IF EXISTS ========"
-kubectl delete pod -n gitlab --all
+helm uninstall -n gitlab gitlab
 kubectl delete namespace gitlab
 
-echo "======== CREATING NAMESPACE IF NOT EXISTS ========"
-kubectl create namespace gitlab
+echo "======== INSTALLING GITLAB ... ========"
 
 helm upgrade --install gitlab ./gitlab --namespace gitlab --create-namespace --wait
-kubectl wait --for=condition=available deployment/gitlab \
-  -l app.kubernetes.io/name=gitlab  \
-  -n gitlab \
-  --timeout=600s
 
-echo "Password is : "
-GITLAB_POD_NAME=$$(kubectl get pods -n gitlab -l app.kubernetes.io/name=gitlab -o jsonpath={.items[0].metadata.name})
+GITLAB_POD_NAME=$(kubectl get pods -n gitlab -l app.kubernetes.io/name=gitlab -o jsonpath={.items[0].metadata.name})
 kubectl exec -n gitlab $GITLAB_POD_NAME -- cat /etc/gitlab/initial_root_password
-echo ""
