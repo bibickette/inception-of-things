13a361ab51227cc5fca904d5bbe20fb85524bc9f
bibickette <imm.taly@gmail.com>
2026-06-08 15:51:21 +0200

    fix(bonus): add timeout for helm

--- bonus/setup.sh
+++ bonus/setup.sh
@@ -13,7 +13,7 @@ k3d cluster create lab --port '8888:80@loadbalancer' --port '8080:443@loadbalanc
 
 echo "======== INSTALLING GITLAB ... ========"
 
-helm upgrade --install gitlab ./gitlab --namespace gitlab --create-namespace --wait
+helm upgrade --install gitlab ./gitlab --namespace gitlab --create-namespace --wait --timeout 10m
 
 GITLAB_POD_NAME=$(kubectl get pods -n gitlab -l app.kubernetes.io/name=gitlab -o jsonpath={.items[0].metadata.name})
 kubectl exec -n gitlab $GITLAB_POD_NAME -- cat /etc/gitlab/initial_root_password
